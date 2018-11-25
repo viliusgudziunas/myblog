31b76176a1d28e664e31ab6320567a1f4a4d9d46
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2018-11-25 15:28:37 +0000

    Added error pages

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,9 @@ def create_app(config_class=Config):
     from app.admin import bp as admin_bp
     app.register_blueprint(admin_bp, url_prefix="/admin")
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
