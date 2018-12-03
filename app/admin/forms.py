ffeeef3acfe4337481cc30e3ceb6a653d161d0e2
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2018-12-03 12:49:09 +0000

    added pages to edit about me and about this blog pages

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -3,11 +3,31 @@ from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
+    """Form for login"""
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Sign In")
 
 class PostForm(FlaskForm):
+    """Form for posts"""
     title = StringField("Create Title", validators=[DataRequired(), Length(max=64)])
     body = TextAreaField("Write Your Post", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+class EditAboutMeForm(FlaskForm):
+    """Form for edit about me page"""
+    body = TextAreaField("About Me", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+    """ def __init__(self, original_body, *args, **kwargs):
+    #     super(EditAboutMeForm, self).__init__(*args, **kwargs)
+    #     self.original_body = original_body"""
+
+class EditAboutThisBlogForm(FlaskForm):
+    """Form for edit about this blog page"""
+    body = TextAreaField("About This Blog", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+    """# def __init__(self, original_body, *args, **kwargs):
+    #     super(EditAboutThisBlogForm, self).__init__(*args, **kwargs)
+    #     self.original_body = original_body"""
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -2,8 +2,8 @@ from werkzeug.urls import url_parse
 from flask import render_template, flash, url_for, redirect, request
 from flask_login import login_required, login_user, current_user
 from app import db
-from app.admin.forms import PostForm, LoginForm
-from app.models import User, Post
+from app.admin.forms import PostForm, LoginForm, EditAboutMeForm, EditAboutThisBlogForm
+from app.models import User, Post, About_Me_Post, About_This_Blog_Post
 from app.admin import bp
 
 @bp.route("/login", methods={"GET", "POST"})
@@ -27,13 +27,13 @@ def login():
 @bp.route("/editorial")
 @login_required
 def editorial():
-    """Admin Editorial page"""
+    """Admin editorial page"""
     return render_template("admin/editorial.html", title="Admin Edtorial")
 
 @bp.route("/editorial/write_post", methods=["GET", "POST"])
 @login_required
 def write_post():
-    """Write Post page"""
+    """Write post page"""
     form = PostForm()
     if form.validate_on_submit():
         post = Post(title=form.title.data, body=form.body.data)
@@ -42,3 +42,29 @@ def write_post():
         flash("The post has been added to the blog!")
         return redirect(url_for("admin.editorial"))
     return render_template("admin/write_post.html", title="Write Post", form=form)
+
+@bp.route("/editorial/write_about_me", methods=["GET", "POST"])
+@login_required
+def write_about_me():
+    """Edit about me page"""
+    form = EditAboutMeForm()
+    if form.validate_on_submit():
+        about_me_post = About_Me_Post(body=form.body.data)
+        db.session.add(about_me_post)
+        db.session.commit()
+        flash("About Me page has been updated.")
+        return redirect(url_for("admin.editorial"))
+    return render_template("admin/write_about_me.html", title="Edit About Me Page", form=form)
+
+@bp.route("/editorial/write_about_this_blog", methods=["GET", "POST"])
+@login_required
+def write_about_this_blog():
+    """Edit about this blog page"""
+    form = EditAboutThisBlogForm()
+    if form.validate_on_submit():
+        about_this_blog_post = About_This_Blog_Post(body=form.body.data)
+        db.session.add(about_this_blog_post)
+        db.session.commit()
+        flash("About This Blog page has been updated.")
+        return redirect(url_for("admin.editorial"))
+    return render_template("admin/write_about_this_blog.html", title="Edit About This Blog Page", form=form)
